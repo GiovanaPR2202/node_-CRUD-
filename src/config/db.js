d09619b41867a5b0b143e39d8820d67dba8bf8ba
giovanaPR2202 <giovanapereira140@gmail.com>
2022-01-05 17:42:44 -0300

    First Commit

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv')
 const path = require('path')
 
-//dotenv.config({path:path.join(__dirname,'../.env')})
+dotenv.config({path:path.join(__dirname,'../.env')})
 
 const db = mysql.createPool({
     connectionLimit: 10,
